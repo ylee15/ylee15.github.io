8060e20e72a6a1ccf76a04ceb60bd2b7a6783077
Yerim Lee <ylee@Yerims-MacBook-Pro.local>
2019-11-11 02:27:58 -0500

    Project 2

--- project-two/assets/hide-reveal.js
+++ project-two/assets/hide-reveal.js
@@ -1,56 +1,65 @@
 $(document).ready(function() {
   //ID or Class name of the button goes into the first section
-  $('#sun-info').click(function() {
+  $('.sun-box').click(function() {
     //ID or Class name of the modal you want to appear on click goes into this second area.
     $(".sun").toggleClass("visible");
     $(".sun-box").toggleClass("invisible");
+    $(".sun .arrow-down").toggleClass("up-animation");
   });
 
-  $('#mercury-info').click(function() {
+  $('.mercury-box').click(function() {
     //ID or Class name of the modal you want to appear on click goes into this second area.
     $(".mercury").toggleClass("visible");
     $(".mercury-box").toggleClass("invisible");
+    $(".mercury .arrow-down").toggleClass("up-animation");
   });
 
-  $('#venus-info').click(function() {
+  $('.venus-box').click(function() {
     //ID or Class name of the modal you want to appear on click goes into this second area.
     $(".venus").toggleClass("visible");
     $(".venus-box").toggleClass("invisible");
+    $(".venus .arrow-down").toggleClass("up-animation");
   });
 
-  $('#earth-info').click(function() {
+  $('.earth-box').click(function() {
     //ID or Class name of the modal you want to appear on click goes into this second area.
     $(".earth").toggleClass("visible");
     $(".earth-box").toggleClass("invisible");
+    $(".earth .arrow-down").toggleClass("up-animation");
   });
 
-  $('#mars-info').click(function() {
+  $('.mars-box').click(function() {
     //ID or Class name of the modal you want to appear on click goes into this second area.
     $(".mars").toggleClass("visible");
     $(".mars-box").toggleClass("invisible");
+    $(".mars .arrow-down").toggleClass("up-animation");
   });
 
-  $('#jupiter-info').click(function() {
+  $('.jupiter-box').click(function() {
     //ID or Class name of the modal you want to appear on click goes into this second area.
     $(".jupiter").toggleClass("visible");
     $(".jupiter-box").toggleClass("invisible");
+    $(".jupiter .arrow-down").toggleClass("up-animation");
   });
 
-  $('#saturn-info').click(function() {
+  $('.saturn-box').click(function() {
     //ID or Class name of the modal you want to appear on click goes into this second area.
     $(".saturn").toggleClass("visible");
     $(".saturn-box").toggleClass("invisible");
+    $(".saturn .arrow-down").toggleClass("up-animation");
   });
 
-  $('#uranus-info').click(function() {
+  $('.uranus-box').click(function() {
     //ID or Class name of the modal you want to appear on click goes into this second area.
     $(".uranus").toggleClass("visible");
     $(".uranus-box").toggleClass("invisible");
+    $(".uranus .arrow-down").toggleClass("up-animation");
   });
 
-  $('#neptune-info').click(function() {
+  $('.neptune-box').click(function() {
     //ID or Class name of the modal you want to appear on click goes into this second area.
     $(".neptune").toggleClass("visible");
     $(".neptune-box").toggleClass("invisible");
+    $(".neptune .arrow-down").toggleClass("up-animation");
   });
 });
